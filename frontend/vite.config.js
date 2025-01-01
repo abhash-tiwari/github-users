2f06c3f1be317af0f93431830798e3d1ff6a62d1
Abhash Tiwari <abhashhtiwary7610@gmail.com>
2025-01-01 23:20:49 +0530

    Testing #3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8000',
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
+
 export default defineConfig({
+  plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://git-users-backend.onrender.com',
-        changeOrigin: true,
-      },
-    },
-  },
+    port : 3000,
+    proxy:{
+      "/api":{
+        target: "http://localhost:8000"
+      }
+    }
+  }
 });
